b91c636c99384cadf6ecca83ac4da1c28bb8c030
ASHISH SUTAR <105456615+Whatyouknow1210@users.noreply.github.com>
2023-09-18 18:39:42 +0530

    Time: 123 ms (94.72%), Space: 91.6 MB (82.44%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,5 +1,8 @@
 class Solution {
     // DP with memoization (make dp same as of return type)
+    /*
+    memset(dp,-1,sizeof dp);
+    return max(f(nums.size()-1,true,nums),f(nums.size()-1,false,nums));
     long long dp[100002][2]; 
     long long f(int i,bool fl,vector<int>&nums)
     {
@@ -16,11 +19,32 @@ class Solution {
         }
         return dp[i][fl] = max(take,f(i-1,fl,nums));
     }
+        */
 public:
     long long maxAlternatingSum(vector<int>& nums) {
-        memset(dp,-1,sizeof dp);
-        return max(f(nums.size()-1,true,nums),f(nums.size()-1,false,nums));
-        
-        
+        int n = nums.size();
+        // Tabulation
+        /*
+        vector<vector<long long>>dp(n+1,vector<long long>(2,0));
+        for(int i=1;i<=n;++i)
+        {
+            dp[i][1] = max(dp[i-1][1],dp[i-1][0]+nums[i-1]);
+            dp[i][0] = max(dp[i][0],dp[i-1][1]-nums[i-1]);
+            dp[i][0] = max(dp[i][0],dp[i-1][0]);
+            dp[i][1] = max(dp[i][1],dp[i-1][1]);
+        }
+        return max(dp[n][0],dp[n][1]);
+        */
+        // long long newEven = max(evensum,oddsum+nums[i]);
+        // long long newOdd = max(oddsum,evensum-nums[i]);
+        // evensum = newEven;
+        // oddsum = newOdd;
+        long long evensum=0,oddsum=0;
+        for(int i=0;i<n;++i)
+        {
+            evensum = max(evensum,oddsum+nums[i]);
+            oddsum = max(oddsum,evensum-nums[i]);
+        }
+        return max(evensum,oddsum);
     }
 };
